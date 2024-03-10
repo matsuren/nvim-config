528ba75f03e8512ba4bd139ecd7a9f9d783fb9bf
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-03-10 17:34:59 +0900

    Modify base settings

--- init.lua
+++ init.lua
@@ -18,19 +18,27 @@ require("lazy").setup("plugins")
 -- Base settings
 vim.opt.history = 3000
 vim.opt.encoding = "utf-8"
+vim.opt.fileformats = "unix,mac,dos"
+vim.opt.swapfile = false
+vim.opt.visualbell = true
+vim.opt.errorbells = true
 vim.opt.cursorline = true
+vim.opt.wrap = false
 vim.opt.number = true
+
+-- Scroll
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 
 -- Tab, indent
 vim.opt.smarttab = true
 vim.opt.expandtab = true        
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0  -- Use tabstop value
 
-
--- Case sensitive \C to disable it
+-- Case sensitive search. \C to disable it
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
-
+vim.opt.wrapscan = true
