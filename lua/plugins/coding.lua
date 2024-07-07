08ae445f6961752967d308df0abff257e669c24e
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-07-07 12:20:16 +0900

    Add treesitter

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,31 @@
+return {
+{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup(
+            {
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "go",
+                    "cmake",
+                    "python",
+                    "rust",
+                    "javascript",
+                    "html",
+                    "arduino",
+                    "markdown",
+                    "markdown_inline"
+                },
+                sync_install = false,
+                highlight = {enable = true},
+                indent = {enable = true}
+            }
+        )
+    end
+}}
