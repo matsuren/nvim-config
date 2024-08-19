d3ced3865dfef93736868af9c89c6ad755a283b4
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-08-19 20:46:29 +0900

    Enable auto_install for tree sitter

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -21,6 +21,7 @@ return {
                     "markdown",
                     "markdown_inline",
                 },
+                auto_install = true,
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
