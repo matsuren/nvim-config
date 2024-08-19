e1167d0496d2b3e866f4454f1363cc944ff31281
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-08-19 21:21:56 +0900

    Add completions

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -68,9 +68,10 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.rust_analyzer.setup({})
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.rust_analyzer.setup({ capabilities = capabilities })
         end,
     },
 }
