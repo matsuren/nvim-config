8a5f4e529746731aa0a1b8a294aa80d9145f72cd
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-08-19 23:55:24 +0900

    Add python setting

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -61,7 +61,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer" },
+				ensure_installed = { "lua_ls", "rust_analyzer", "ruff", "pyright" },
 			})
 		end,
 	},
@@ -71,6 +71,8 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.ruff.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
 			-- lspconfig.rust_analyzer.setup({ capabilities = capabilities }) -- rustaceanvim handle this part
 			vim.lsp.inlay_hint.enable(true)
 		end,
@@ -79,5 +81,22 @@ return {
 		"mrcjkb/rustaceanvim",
 		version = "^5", -- Recommended
 		lazy = false, -- This plugin is already lazy
+	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			"mfussenegger/nvim-dap-python", --optional
+			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		},
+		lazy = false,
+		branch = "regexp", -- This is the regexp branch, use this for the new version
+		config = function()
+			require("venv-selector").setup()
+		end,
+		keys = {
+			{ "<leader>cv", "<cmd>VenvSelect<cr>" },
+		},
 	},
 }
