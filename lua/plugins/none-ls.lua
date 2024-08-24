66e2ab259ba89ea23a7bcf2ac1ef9022b48adab8
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-08-24 22:04:25 +0900

    Change lua formatter from tabs to spaces

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,9 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.stylua.with({
+                    extra_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                }),
             },
         })
     end,
