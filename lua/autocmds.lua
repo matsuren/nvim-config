4580f5e25fe48f62fa6c17d5f86fa59dd9b8ae62
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-08-28 22:40:00 +0900

    Handle telescope wrong insert mode apearing issue after picking file

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -11,3 +11,13 @@ vim.api.nvim_create_autocmd("CursorHold", {
     pattern = "*",
     command = "checktime",
 })
+
+-- Handle insert mode apearing issue
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2766
+vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+        end
+    end,
+})
