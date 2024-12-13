53ee3efda37c4ed654fe7dd141aff3394d9d78d8
Ren Komatsu <ren.komatsu.rk@gmail.com>
2024-12-13 22:44:45 +0900

    Fix typo

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -319,7 +319,7 @@ return {
         "LintaoAmons/scratch.nvim",
         event = "VeryLazy",
         dependencies = {
-            { "ibhagwan/fzf-lua" }, --optional: if you want to use fzf-lua to pick scratch file. Recommanded, since it will order the files by modification datetime desc. (require rg)
+            { "ibhagwan/fzf-lua" }, --optional: if you want to use fzf-lua to pick scratch file. Recommended, since it will order the files by modification datetime desc. (require rg)
             -- { "nvim-telescope/telescope.nvim" }, -- optional: if you want to pick scratch file by telescope
             -- { "stevearc/dressing.nvim" }, -- optional: to have the same UI shown in the GIF
         },
@@ -328,7 +328,7 @@ return {
                 scratch_file_dir = vim.fn.stdpath("cache") .. "/scratch.nvim", -- where your scratch files will be put
                 window_cmd = "rightbelow vsplit", -- 'vsplit' | 'split' | 'edit' | 'tabedit' | 'rightbelow vsplit'
                 use_telescope = true,
-                -- fzf-lua is recommanded, since it will order the files by modification datetime desc. (require rg)
+                -- fzf-lua is recommended, since it will order the files by modification datetime desc. (require rg)
                 file_picker = "fzflua", -- "fzflua" | "telescope" | nil
                 filetypes = { "py", "lua", "js", "sh", "ts" }, -- you can simply put filetype here
             })
