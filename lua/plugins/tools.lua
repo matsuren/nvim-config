ca0e6fee0626720955556dc9102698677b1d627a
Ren Komatsu <ren.komatsu.rk@gmail.com>
2025-08-05 23:24:18 +0900

    Pin version for Kulala

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -3,6 +3,7 @@ return {
     -- (optional) go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
     {
         "mistweaverco/kulala.nvim",
+        version = "4.*",
         ft = { "http", "rest" },
         opts = {
             global_keymaps = false, -- Set keymap in autocmds,lua
