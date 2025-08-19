bc9dbd6bddfebc6440f05bfcb3a830438b4c0101
Ren Komatsu <ren.komatsu.rk@gmail.com>
2025-08-19 21:59:59 +0900

    Add vue lsp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -240,6 +240,26 @@ return {
             lspconfig.zls.setup({ capabilities = capabilities })
             -- lspconfig.rust_analyzer.setup({ capabilities = capabilities }) -- rustaceanvim handle this part
             -- lspconfig.jsonls.setup({ capabilities = capabilities })
+            local vue_language_server_path = vim.fn.stdpath("data")
+                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+            local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+            local vue_plugin = {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue" },
+                configNamespace = "typescript",
+            }
+            local ts_ls_config = {
+                init_options = {
+                    plugins = {
+                        vue_plugin,
+                    },
+                },
+                filetypes = tsserver_filetypes,
+                capabilities = capabilities,
+            }
+            lspconfig.ts_ls.setup(ts_ls_config)
+            lspconfig.vue_ls.setup({})
             vim.lsp.inlay_hint.enable(true)
             local base_severity = vim.diagnostic.severity.HINT
             vim.diagnostic.config({
