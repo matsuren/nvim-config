6e68751dcc9bac83cf3de7179a6d614f218acf10
Ren Komatsu <ren.komatsu.rk@gmail.com>
2026-03-18 21:01:05 +0900

    fix: lsp.progress.enabled = false to noice.nvim config so only fidget.nvim shows progress bar

--- lua/plugins/appearances.lua
+++ lua/plugins/appearances.lua
@@ -215,6 +215,9 @@ return {
                     view_search = false,
                 },
                 lsp = {
+                    progress = {
+                        enabled = false,
+                    },
                     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
                     override = {
                         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
