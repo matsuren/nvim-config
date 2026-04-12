6542e8a55f155ca004f42ac1e59dc5d2c06a61b1
Ren Komatsu <ren.komatsu.rk@gmail.com>
2026-04-12 10:15:20 +0900

    chore(noice): disable notify to avoid conflict

--- lua/plugins/appearances.lua
+++ lua/plugins/appearances.lua
@@ -239,6 +239,9 @@ return {
                     inc_rename = false, -- enables an input dialog for inc-rename.nvim
                     lsp_doc_border = true, -- add a border to hover docs and signature help
                 },
+                notify = {
+                    enabled = false,
+                },
             })
         end,
     },
